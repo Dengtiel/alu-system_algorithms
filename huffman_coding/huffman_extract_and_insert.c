94caaf0398218977ad60e6d5bd980069a878a4bd
Dengtiel <d.akol@alustudent.com>
2026-05-31 22:21:32 +0200

    Task 7: fix Betty style - split function under 40 lines

--- huffman_coding/huffman_extract_and_insert.c
+++ huffman_coding/huffman_extract_and_insert.c
@@ -2,6 +2,44 @@
 #include "huffman.h"
 
 /**
+ * create_combined - Creates a combined parent node from two extracted nodes
+ *
+ * @left: Left child node
+ * @right: Right child node
+ *
+ * Return: New parent binary_tree_node_t, or NULL on failure
+ */
+static binary_tree_node_t *create_combined(binary_tree_node_t *left,
+	binary_tree_node_t *right)
+{
+	symbol_t *symbol_left, *symbol_right, *symbol_new;
+	binary_tree_node_t *parent;
+	size_t combined_freq;
+
+	symbol_left = (symbol_t *)left->data;
+	symbol_right = (symbol_t *)right->data;
+	combined_freq = symbol_left->freq + symbol_right->freq;
+
+	symbol_new = symbol_create(-1, combined_freq);
+	if (symbol_new == NULL)
+		return (NULL);
+
+	parent = binary_tree_node(NULL, symbol_new);
+	if (parent == NULL)
+	{
+		free(symbol_new);
+		return (NULL);
+	}
+
+	parent->left = left;
+	parent->right = right;
+	left->parent = parent;
+	right->parent = parent;
+
+	return (parent);
+}
+
+/**
  * huffman_extract_and_insert - Extracts two nodes from priority queue
  * and inserts a new combined node
  *
@@ -12,13 +50,10 @@
 int huffman_extract_and_insert(heap_t *priority_queue)
 {
 	binary_tree_node_t *left, *right, *parent;
-	symbol_t *symbol_left, *symbol_right, *symbol_new;
-	size_t combined_freq;
 
 	if (priority_queue == NULL)
 		return (0);
 
-	/* Extract two least frequent nodes */
 	left = (binary_tree_node_t *)heap_extract(priority_queue);
 	if (left == NULL)
 		return (0);
@@ -27,34 +62,13 @@ int huffman_extract_and_insert(heap_t *priority_queue)
 	if (right == NULL)
 		return (0);
 
-	/* Get their frequencies */
-	symbol_left = (symbol_t *)left->data;
-	symbol_right = (symbol_t *)right->data;
-	combined_freq = symbol_left->freq + symbol_right->freq;
-
-	/* Create new symbol with combined frequency */
-	symbol_new = symbol_create(-1, combined_freq);
-	if (symbol_new == NULL)
-		return (0);
-
-	/* Create new nested node as parent of left and right */
-	parent = binary_tree_node(NULL, symbol_new);
+	parent = create_combined(left, right);
 	if (parent == NULL)
-	{
-		free(symbol_new);
 		return (0);
-	}
 
-	/* Set left and right children */
-	parent->left = left;
-	parent->right = right;
-	left->parent = parent;
-	right->parent = parent;
-
-	/* Insert new nested node back into priority queue */
 	if (heap_insert(priority_queue, parent) == NULL)
 	{
-		free(symbol_new);
+		free(parent->data);
 		free(parent);
 		return (0);
 	}
